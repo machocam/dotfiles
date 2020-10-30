72b460f4b40b3a82a3cb4037d0bb231236452555
mixcocam <camachorod@gmail.com>
2020-10-30 11:08:36 -0600

    added ripgrep for fzf bindings

--- InstallLinuxApps.sh
+++ InstallLinuxApps.sh
@@ -3,7 +3,7 @@
 
 ## From standard repos
 
-sudo apt install vlc snapd git curl keepassxc ssh syncthing fzf
+sudo apt install vlc snapd git curl keepassxc ssh syncthing fzf ripgrep
 
 ## Snap packages
 sudo snap install discord spotify signal-desktop 
