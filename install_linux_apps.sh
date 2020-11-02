96955b3c356a1a2f35d72fb150b1317ab299fd90
mixcocam <camachorod@gmail.com>
2020-11-02 15:13:40 -0600

    added htop to the list, man it only gets longer!

--- install_linux_apps.sh
+++ install_linux_apps.sh
@@ -4,7 +4,7 @@
 
 ## From standard repos
 
-sudo apt install vlc snapd git curl keepassxc ssh syncthing fzf ripgrep newsboat
+sudo apt install vlc snapd git curl keepassxc ssh syncthing fzf ripgrep newsboathtop
 
 ## Snap packages
 sudo snap install discord spotify signal-desktop 
