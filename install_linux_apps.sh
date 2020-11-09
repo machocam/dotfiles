8dac832a5be3ecfd30ff20c8e763f40df053c8db
mixcocam <camachorod@gmail.com>
2020-11-09 17:18:01 -0600

    added pgp, can't believe it doesn't come with install

--- install_linux_apps.sh
+++ install_linux_apps.sh
@@ -4,7 +4,7 @@
 
 ## From standard repos
 
-sudo apt install vlc snapd git curl keepassxc ssh syncthing fzf ripgrep newsboathtop neovim pandoc
+sudo apt install vlc snapd git curl keepassxc ssh syncthing fzf ripgrep newsboathtop neovim pandoc pgp
 
 ## Snap packages
 sudo snap install discord spotify signal-desktop 
