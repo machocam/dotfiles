294125295a05bed59c7ec55baeab4136898ef179
mixcocam <camachorod@gmail.com>
2020-11-12 19:32:30 -0600

    added universal-ctags inspired by this https://www.edwinwenink.xyz/posts/43-notes_tagging/

--- install_linux_apps.sh
+++ install_linux_apps.sh
@@ -4,7 +4,7 @@
 
 ## From standard repos
 
-sudo apt install vlc snapd git curl keepassxc ssh syncthing fzf ripgrep newsboathtop neovim pandoc pgp
+sudo apt install vlc snapd git curl keepassxc ssh syncthing fzf ripgrep newsboathtop neovim pandoc pgp universal-ctags 
 
 ## Snap packages
 sudo snap install discord spotify signal-desktop 
